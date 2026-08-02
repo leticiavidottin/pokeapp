8c4016e82202056c912d49f16f5ef630340e489e
leticiavidottin <leticiavidottin@gmail.com>
2026-08-02 17:58:38 -0300

    feat: commit inicial

---
+++ src/main/java/com/pokeapp/pokebaralho_api/PokebaralhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.pokeapp.pokebaralho_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokebaralhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokebaralhoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokeapp/pokebaralho_api/PokebaralhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeapp.pokebaralho_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokebaralhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
